83632bcdb301f9a1310e57eb6cfeb01fdc952118
ahssano1 <ahssano1@naver.com>
2022-06-08 11:17:16 +0900

    joijqw1f

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://127.0.0.1:8090/',
+	baseURL: 'https://jimingeuncom.koreacentral.cloudapp.azure.com/',
 })
 
 instance.defaults.headers.post['Content-Type'] =
